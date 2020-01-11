d8cf76e6f7a145a146c048019c1549c62994cf08
chase1635321 <chasekanipe@gmail.com>
2020-01-11 11:44:13 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # radare2-scripts
+ - List leaf functions, rename leaf functions
+ - Dump function with pdg or pdf, compare with known syscall signatures
+
---
+++ list_leaves.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import r2pipe
+import sys
+import Levenshtein as Leven
+
+if len(sys.argv) < 2:
+    print("Usage: ./leafs <binary>")
+
+r = r2pipe.open(sys.argv[1])
+
+r.cmd("aaa")
+
+r.cmd("i~static")
+
+functions = r.cmd("aflt | awk '$12<1' | awk '{print $6}'").split("\n")
+functions = [i for i in functions if i]
+
+print("Found " + str(len(functions)) + " leaf functions")
+
+for function in functions:
+    print(function)
+
