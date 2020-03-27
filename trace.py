9334510be736aaaab79624cfa702eb07b44f43e7
chase1635321 <chasekanipe@gmail.com>
2020-03-27 18:40:47 +0000

    Automatic push

--- trace.py
+++ trace.py
@@ -6,6 +6,8 @@ import os
 
 r = r2pipe.open()
 
+os.system("rm trace_cache.txt")
+
 cache = ""
 try:
     with open("trace_cache.txt") as f:
@@ -17,6 +19,7 @@ except:
 
     functionData = []
 
+    arglist = ""
     for a in functions:
         if a['size'] > 30:
             print("[+] Saving function " + colored(a['name'], "green") + " of size " + str(a['size']))
@@ -26,10 +29,39 @@ except:
 
             for line in r.cmd("pdg").split("\n"):
                 if "(" in line and ")" in line and line[0] != " " and not "ram" in line:
+                    arglist += hex(a['offset']) + "," + line.split("(")[1].split(")")[0] + "\n"
                     functionData.append(hex(a['offset']) + ";" + line)
 
+    with open("temp.txt", "w") as f:
+        f.write(arglist)
+
+    os.system("""cat temp.txt | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > temp2.txt""")
+
+    with open("temp2.txt", "r") as f:
+        arglist = f.read().split("\n")
+
+    argCommands = []
+    for line in arglist:
+        commands = ""
+        addr = line.split(",")[0]
+        r.cmd("s " + addr)
+        other = line.split(",")[1:]
+        for arg in other:
+            temp = r.cmd("afvd " + arg.split(" ")[-1])
+            if len(temp) > 1:
+                commands += ";" + temp.strip()
+        if commands == "":
+            commands = "\n"
+
+        argCommands.append(commands)
+    print(str(argCommands))
+    
+    finalCache = []
+    for i in range(0, len(functionData)):
+        finalCache.append(functionData[i] + argCommands[i].strip())
+
     with open("trace_cache.txt", "w") as f:
-        f.write("\n".join(functionData))
+        f.write("\n".join(finalCache))
 
     os.system("cat trace_cache.txt")
     print(colored("\nCache generated, run script again to trace program execution", "green"))
